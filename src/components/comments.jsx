c444af91d92604dfa6d0e9072b7a28fa637ff4ef
Laura <laurakcleve@gmail.com>
2019-11-05 12:13:37 -0700

    Commento styling

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,14 +1,19 @@
 import { Helmet } from 'react-helmet'
 import React from 'react'
 
+import styles from '../styles/commento-custom.css'
+
 const Comments = () => {
   return (
     <>
-      <div id="commento">
-        <Helmet>
-          <script defer src="https://cdn.commento.io/js/commento.js" />
-        </Helmet>
-      </div>
+      <div id="commento" />
+      <Helmet>
+        <script
+          defer
+          src="https://cdn.commento.io/js/commento.js"
+          data-css-override={styles}
+        />
+      </Helmet>
     </>
   )
 }
