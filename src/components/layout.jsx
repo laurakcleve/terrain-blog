7c2b8065df394aa271e3534855845eb64df8b032
Laura <laurakcleve@gmail.com>
2019-11-08 14:56:21 -0700

    Contact form and some styling

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,15 +4,23 @@ import PropTypes from 'prop-types'
 
 import '../styles/styles.scss'
 import Logo from '../../content/assets/logo.svg'
+import Line from '../../content/assets/line.svg'
 
 const Layout = ({ children }) => {
   return (
     <>
       <header>
         <div className="wrapper">
-          <Link to="/" className="logo">
-            <Logo />
-          </Link>
+          <div>
+            <Link to="/" className="logo">
+              <Logo />
+            </Link>
+            <Line className="line" />
+            <div className="tagline">TABLETOP DIY</div>
+          </div>
+          <nav>
+            <Link to="/about">About</Link>
+          </nav>
         </div>
       </header>
       <main>{children}</main>
---
+++ src/pages/about.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <p>
+      My name is Laura, and I&#39;m a new recruit to the world of tabletop gaming.
+      Building terrain is my jam. I also love miniature painting, and plan to get
+      some pictures up sometime soon.
+    </p>
+    <p>Shoot me a message here:</p>
+    <form method="POST" data-netlify="true">
+      <label htmlFor="name">
+        <span>Name</span>
+        <input type="text" name="name" />
+      </label>
+
+      <label htmlFor="email">
+        <span>Email address</span>
+        <input type="text" name="email" />
+      </label>
+
+      <label htmlFor="message">
+        <span>Message</span>
+        <textarea name="message" />
+      </label>
+
+      <button type="submit">Send</button>
+    </form>
+  </Layout>
+)
+
+export default About
